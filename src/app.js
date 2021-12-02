c7f2460fe38b7288256c1d85dc8e953dfcdff9f5
Ankur Kundu <ankur@codelogicx.com>
2021-12-02 14:00:19 +0530

    Heroku App setup

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const weather = require('./utils/forecast')
 const geolocation = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define path for express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -69,6 +70,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000.')
+app.listen(port, () => {
+    console.log('Server started on port '+port+'.')
 })
